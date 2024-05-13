401588fa7d02c52e74b4eca0cef8f9d9359246a6
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-05-13 15:53:49 +0200

    worked on chat section and fix the Routers

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,26 @@ import List from "./components/pages/List";
 import Details from "./components/pages/Details";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 import Chat from "./components/pages/chat-Pages/Chat";
+import Layout from "./components/layouts/Layout";
 
 function App() {
   return (
-    <>
-      <div className="main-layout">
+    <> 
+      
         <Router>
-          <Header />
-
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/List" element={<List />} />
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/List" element={<List />} />
+              <Route path="/Details" element={<Details />} />
+            </Route>
+
             <Route path="/Chat" element={<Chat />} />
-            <Route path="/Details" element={<Details />} />
             <Route path="/Register" element={<Register />} />
             <Route path="/Login" element={<Login />} />
           </Routes>
-          <Footer />
         </Router>
-      </div>
     </>
   );
 }
