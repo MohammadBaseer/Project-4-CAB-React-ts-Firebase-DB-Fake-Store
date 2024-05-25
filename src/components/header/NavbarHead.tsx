e75fc526762402144102d3005784b6b86fe3033e
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-05-25 02:52:29 +0200

    Profile photos design fixed

--- src/components/header/NavbarHead.tsx
+++ src/components/header/NavbarHead.tsx
@@ -50,6 +50,16 @@ const NavbarHead = () => {
                   }
                 >
                   <div className="user-tab-navbar-element">
+                  <img
+                    className="user-photo"
+                    src="https://st3.depositphotos.com/15648834/17930/v/450/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                    alt=""
+                    onClick={toggle}
+                  />
+                  <p>Mohammad Baseer Rahimi</p>
+                </div>
+
+                  <div className="user-tab-navbar-element">
                     <span className="pi pi-user">
                       <Link to="/register"> My Profile</Link>
                     </span>
